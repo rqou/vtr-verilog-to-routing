c4df7703d0c339a8d6ed6b48125a7e295c63550b
Robert Ou <rqou@robertou.com>
2018-07-17 23:32:56 -0700

    begin testing register

--- maxv/test.v
+++ maxv/test.v
@@ -1,7 +1,13 @@
-module test(input a, input b, output o_and, output o_or, output o_xor);
+module test(input a, input b, output o_and, output o_or, output o_xor, input clk);
 
-assign o_and = a & b;
-assign o_or = a | b;
-assign o_xor = a ^ b;
+// assign o_and = a & b;
+// assign o_or = a | b;
+// assign o_xor = a ^ b;
+
+always @(posedge clk) begin
+    o_and = a & b;
+    o_or = a | b;
+    o_xor = a ^ b;
+end
 
 endmodule
